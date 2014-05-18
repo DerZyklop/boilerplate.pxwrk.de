14a77342cccb764e51e3e71740a03133a13f0ffb
Nils Neumann <mail@der-zyklop.de>
2014-05-18 16:44:56 +0200

    more bower-stuff

--- set_bower_stuff.sh
+++ set_bower_stuff.sh
@@ -1,4 +1,4 @@
 ln -s bower_components/boilerplate.pxwrk.de/gruntfile.coffee ./
-cp -s bower_components/boilerplate.pxwrk.de/paths.json ./
-cp -s bower_components/boilerplate.pxwrk.de/eslint.json ./
-cp -s bower_components/boilerplate.pxwrk.de/package.json ./
+cp bower_components/boilerplate.pxwrk.de/paths.json ./
+cp bower_components/boilerplate.pxwrk.de/eslint.json ./
+cp bower_components/boilerplate.pxwrk.de/package.json ./
