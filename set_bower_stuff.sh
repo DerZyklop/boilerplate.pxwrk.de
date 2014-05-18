bcadbb4fb5202bee3dc25e9937489e01eac2fac2
Nils Neumann <mail@der-zyklop.de>
2014-05-18 16:49:07 +0200

    -

--- set_bower_stuff.sh
+++ set_bower_stuff.sh
@@ -1,4 +1,4 @@
 ln -fs bower_components/boilerplate.pxwrk.de/gruntfile.coffee
-cp bower_components/boilerplate.pxwrk.de/paths.json
-cp bower_components/boilerplate.pxwrk.de/eslint.json
-cp bower_components/boilerplate.pxwrk.de/package.json
+cp bower_components/boilerplate.pxwrk.de/paths.json .
+cp bower_components/boilerplate.pxwrk.de/eslint.json .
+cp bower_components/boilerplate.pxwrk.de/package.json .
