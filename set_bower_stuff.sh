3db2b5e95aaecb4e06188c5f009ce09b465e061a
DerZyklop <mail@der-zyklop.de>
2014-08-05 17:04:08 +0200

    Delete set_bower_stuff.sh

--- set_bower_stuff.sh
+++
@@ -1,4 +0,0 @@
-ln -fs bower_components/boilerplate.pxwrk.de/gruntfile.coffee
-cp bower_components/boilerplate.pxwrk.de/paths.json .
-cp bower_components/boilerplate.pxwrk.de/eslint.json .
-cp bower_components/boilerplate.pxwrk.de/package.json .
